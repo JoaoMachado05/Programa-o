a3f7ff9c922401955ac197f5ff7accd71c8ad634
Tomás <josetomasribeirosilva@gmail.com>
2025-02-23 11:11:04 +0000

    correcao nomes ficheiros

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import Login from './pages/Login/App.jsx'
+import Login from './pages/Login/Login.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
