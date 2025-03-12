33185efe65efff80fec69e497e4e8b5032eb00f7
JeNotSabere <a108633@alunos.uminho.pt>
2025-03-12 15:36:09 +0000

    olá :D

---
+++ src/main/java/pt/LEGSI_DAI_PL1_G1/TUB_Digital_Twins/SecurityConfig.java
@@ -0,0 +1,15 @@
+package pt.LEGSI_DAI_PL1_G1.TUB_Digital_Twins;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return web -> web.ignoring().requestMatchers("/**");
+    }
+}
