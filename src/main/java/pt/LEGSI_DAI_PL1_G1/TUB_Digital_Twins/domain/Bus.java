53a414d68e9228825272a9122a9f021324443691
barbara687 <a108631@uminho.pt>
2025-04-26 20:36:26 +0100

    Parte Miguel

--- src/main/java/pt/LEGSI_DAI_PL1_G1/TUB_Digital_Twins/domain/Bus.java
+++ src/main/java/pt/LEGSI_DAI_PL1_G1/TUB_Digital_Twins/domain/Bus.java
@@ -5,6 +5,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "buses", uniqueConstraints = {@UniqueConstraint(columnNames = "matricula")})
 @Data
@@ -32,4 +36,56 @@ public class Bus {
     private Double temperaturaAtual;
     private Double latitude;
     private Double longitude;
+
+    
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private StatusBus status = StatusBus.OPERACIONAL;
+
+    private LocalDateTime proximaParagemHorario;
+    private Integer atrasoMinutos = 0;
+
+    @ManyToMany
+    @JoinTable(
+            name = "bus_rota",
+            joinColumns = @JoinColumn(name = "bus_id"),
+            inverseJoinColumns = @JoinColumn(name = "rota_id"))
+    private List<Rota> rotas = new ArrayList<>();
+
+    // Novos atributos para temperatura e ação
+    private Float lastTemperature;
+    private String temperatureAction;
+
+    // Constructor com todos os parâmetros
+    public Bus(Long id, String matricula, Integer capacidadeMaxima, Integer lotacaoAtual, String linhaAtual, Double velocidade, Double temperaturaAtual, Double latitude, Double longitude) {
+        this.id = id;
+        this.matricula = matricula;
+        this.capacidadeMaxima = capacidadeMaxima;
+        this.lotacaoAtual = lotacaoAtual;
+        this.linhaAtual = linhaAtual;
+        this.velocidade = velocidade;
+        this.temperaturaAtual = temperaturaAtual;
+        this.latitude = latitude;
+        this.longitude = longitude;
+        this.proximaParagemHorario = proximaParagemHorario;
+        this.atrasoMinutos = atrasoMinutos;
+        this.lastTemperature = lastTemperature;
+        this.temperatureAction = temperatureAction;
+    }
+
+    public void setLastTemperature(Float lastTemperature) {
+        this.lastTemperature = lastTemperature;
+    }
+
+    public void setTemperatureAction(String temperatureAction) {
+        this.temperatureAction = temperatureAction;
+    }
+
+    // Enum para o status do autocarro
+    public enum StatusBus {
+        OPERACIONAL,
+        EM_MANUTENCAO,
+        AVARIADO,
+        FORA_DE_SERVICO
+    }
 }
