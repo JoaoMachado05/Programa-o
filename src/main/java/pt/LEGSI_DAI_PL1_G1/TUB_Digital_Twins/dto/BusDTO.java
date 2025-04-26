d1a7462a32172346bcbcf6bdfa52746c72a97baf
barbara687 <a108631@uminho.pt>
2025-04-26 20:38:09 +0100

    Parte Miguel

--- src/main/java/pt/LEGSI_DAI_PL1_G1/TUB_Digital_Twins/dto/BusDTO.java
+++ src/main/java/pt/LEGSI_DAI_PL1_G1/TUB_Digital_Twins/dto/BusDTO.java
@@ -1,14 +1,86 @@
 package pt.LEGSI_DAI_PL1_G1.TUB_Digital_Twins.dto;
 
-public record BusDTO(
-        Long id,
-        String matricula,
-        Integer capacidadeMaxima,
-        Integer lotacaoAtual,
-        Double percentagemOcupacao,
-        String linhaAtual,
-        Double velocidade,
-        Double temperaturaAtual,
-        Double latitude,
-        Double longitude) {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class BusDTO {
+    private Long id;
+    private String matricula;
+    private Integer capacidadeMaxima;
+    private Integer lotacaoAtual;
+    private String linhaAtual;
+    private Double velocidade;
+    private Double temperaturaAtual;
+    private Double latitude;
+    private Double longitude;
+    private LocalDateTime proximaParagemHorario;
+    private Integer atrasoMinutos;
+
+
+    // Novos campos
+    private Float lastTemperature;
+    private String temperatureAction;
+
+    public BusDTO(Long id, String matricula, Integer capacidadeMaxima, Integer lotacaoAtual, Double percentagemOcupacaoAtual, String linhaAtual, Double velocidade, Double temperaturaAtual, Double latitude, Double longitude) {
+
+    }
+
+    // Métodos getters e setters
+    public Long id() {
+        return this.id;
+    }
+
+    public String matricula() {
+        return this.matricula;
+    }
+
+    public Integer capacidadeMaxima() {
+        return this.capacidadeMaxima;
+    }
+
+    public Integer lotacaoAtual() {
+        return this.lotacaoAtual;
+    }
+
+    public String linhaAtual() {
+        return this.linhaAtual;
+    }
+
+    public Double velocidade() {
+        return this.velocidade;
+    }
+
+    public Double temperaturaAtual() {
+        return this.temperaturaAtual;
+    }
+
+    public Double latitude() {
+        return this.latitude;
+    }
+
+    public Double longitude() {
+        return this.longitude;
+    }
+
+    public LocalDateTime proximaParagemHorario() {
+        return this.proximaParagemHorario;
+    }
+
+    public Integer atrasoMinutos() {
+        return this.atrasoMinutos;
+    }
+
+    public Float lastTemperature() {
+        return this.lastTemperature;
+    }
+
+    public String temperatureAction() {
+        return this.temperatureAction;
+    }
 }
